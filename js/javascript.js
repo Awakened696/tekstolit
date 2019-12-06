335bdc22e7a2dbe794a959611993e625aef1d82d
Max <51928996+Awakened696@users.noreply.github.com>
2019-12-06 17:21:08 +0300

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,59 @@
+$('.slider_slick').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3
+});
+
+$('.toggle-block').click(function() {
+  if ($('.block').is(':visible')) {
+    $('.block').hide('slow');
+  }
+  else {
+    $('.block').show('slow');
+  }
+});
+
+$('.toggle-block_2').click(function() {
+  if ($('.block_2').is(':visible')) {
+    $('.block_2').hide('slow');
+  }
+  else {
+    $('.block_2').show('slow');
+  }
+});
+
+$('.toggle-block_3').click(function() {
+  if ($('.block_3').is(':visible')) {
+    $('.block_3').hide('slow');
+  }
+  else {
+    $('.block_3').show('slow');
+  }
+});
+
+$('.toggle-block_4').click(function() {
+  if ($('.block_4').is(':visible')) {
+    $('.block_4').hide('slow');
+  }
+  else {
+    $('.block_4').show('slow');
+  }
+});
+
+$('.toggle-block_5').click(function() {
+  if ($('.block_5').is(':visible')) {
+    $('.block_5').hide('slow');
+  }
+  else {
+    $('.block_5').show('slow');
+  }
+});
+
+$('.toggle-block_6').click(function() {
+  if ($('.block_6').is(':visible')) {
+    $('.block_6').hide('slow');
+  }
+  else {
+    $('.block_6').show('slow');
+  }
+});
